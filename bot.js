fa2a63486a700ab1919db3e3f153f89ec4156ad3
unknown <kiriasuna710@gmail.com>
2018-07-17 18:48:07 -0700

    new libraries and gatcha roll

--- bot.js
+++ bot.js
@@ -1,196 +1,229 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, { colorize: true });
-
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+// Import the discord.js module
+const Discord = require('discord.js');
+const economy = require('discord-eco');
+const fs = require("fs");
+// Create an instance of a Discord client
+const client = new Discord.Client();
+const token = 'NDYyNzkxOTc1NDkwNTUxODA4.DhnAKw.AkURR8jb6MKwsQDybtQ7zKTdED0';
+let stats = JSON.parse(fs.readFileSync("stats.json", "utf8"));
+// Quick simulation for gatcha
+client.on('ready', () => {
+	var  i1 =0 , i2=0, i3=0, i4=0, i5=0;
+	for ( var i = 0; i < 1000; i++) { 
+		var starrandom = Math.floor(Math.random() * 99);
+		if(starrandom >= 0 && starrandom <=7) i5++;
+		if(starrandom >= 7 && starrandom <=20) i4++;
+		if(starrandom >= 43 && starrandom <=70) i3++;
+		if(starrandom >= 43 && starrandom <=70) i2++;
+		if(starrandom >= 71 && starrandom <=99) i1++;
+	}
+	console.log('1 stars *' + i1);
+	console.log('2 stars *' +i2);
+	console.log('3 stars *' +i3);
+	console.log('4 stars *' +i4);
+	console.log('5 stars *' +i5);
+	console.log('I am ready!☆☆☆☆☆');
 });
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
 
-var chickysaying = [
-	"ye unless you're doing stuff atm, can run after but I gotta fit in another sell after chicky too",
-	"Still in bed and gotta run with chicky x,x",
-	"oh uhg running pub atm and need to sell witrh chicky after before he passes out",
-	"O i gotta run three commie raids and run with chicky",
-	"running 4/6th raid, gotta run chicky first",
-	"I'll probably run with chicky after this and then we can run if you want andrew",
-	"gonna just finish eating, chicky needs to run five raids atm so we can fit ours in the meanwhile shortly if you want",
-	"o wait I didn't run with chicky yet",
-	"if he can run now AI can tell chicky just like 15min",
-	"ran our last raid with chicky, my bm is free now :sob:",
-	"lemme know if you wnana run; gotta run with chicky in 50minish-ish",
-]
+////////////// --------- number of possiblities
+var star1pics = 95;
+var star2pics = 132;
+var star3pics = 141;
+var star4pics = 137;
+var star5pics = 132;
 
-var damiesayings = [
-	"DAMIEBUNS"
-]
-
-var sleepsaying = [
-	"I sleep max like probably ten hours",
-	"I am going back to sleep now",
-	"Zero hours sleep again",
-	"BETTER GO SLEEP before my sleep randomly dies again",
-	"omg upset stomach can't sleep yet :coffin:",
-	"Almost didn't sleep again too me like at least two hours",
-	"alright itme to like sleep NIGHTNIGHT",
-	"i got very unknown and very bad sleep again rofl",
-	"Cannot sleep for no reason like always",
-	"it took me like two three hours to sleep for no reason and then random heavy 15min shower woke me at the end rofl",
-	"probably ded today just lost too much sleep ROFL",
-	"Ded i think i missed sleep train",
-	"sleep is too powerful men",
-	"my only pleasure comes from the feeling I am getting better with sleeps",
-	"lets go sleep",
-	"too late almost time for sleep :ghost:",
-	"Omg there's birds chirping time to sleep",
-	"Hihi time for sleep",
-	"wtf itme to sleep",
-	"but I gotta go sleep like really soon x_x",
-]
-
-bot.on('message', function (user, userID, channelID, message, evt) 
+client.on('message', message => 
 {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-		var eat = args[1];
-       
-        args = args.splice(1);
-        switch(cmd) 
-			{
-			case 'help':
-                bot.sendMessage
-				({
-                    to: channelID,
-                    message: '!nou \n !linami\n !oliver\n !goodboy\n !qtpie \n !sleep\n !csgo\n !old\n !wanna __\n !work\n !albert\n !luv\n !hate\n !chicky\n' 
-                });
-				break;
-			case 'sleep':
-				var sleepran = Math.floor(Math.random() * sleepsaying.length); 
-				bot.sendMessage
-				({
-                    to: channelID,
-                    message: sleepsaying[sleepran]
-                });
-			break;
-			case 'albepls':
-				bot.sendMessage
-				({
-                    to: channelID,
-                    message: 'I think m.2 ssds are 4x or something faster than sata 600mb/s \nidk how that affects iops and small file processing speed tho \n "what\'s the difference between m.2 and pcie drives" \nthat I got no clu'
-                });
-				break;
-			case 'albert':
-                bot.sendMessage
-				({
-                    to: channelID,
-                    message: 'albe go sleep ok'
-                });
-				break;
-			case 'luv':
-                bot.sendMessage
-				({
-                    to: channelID,
-                    message: 'I LUV U'
-                });
-				break;
-			case 'linami':
-                bot.sendMessage
-				({
-                    to: channelID,
-                    message: 'linami-chaaaaaan :sob:'
-                });
-				break;
-			case 'wanna':
-                bot.sendMessage
-				({
-                    to: channelID,
-                    message: 'i wanna ' + eat + ' u!'
-                });
-				break;
-			case 'csgo':
-                bot.sendMessage
-				({
-                    to: channelID,
-                    message: 'and I really absolkutely hate ad strafing\n it\'s not even like quake with more linear movement\njust people randomly stopping and instantly moving again at 100% speed\nana only feels good because of her duality\ncsgo is just the better game\ni dont know'
-                });
-				break;
-			case 'old':
-                bot.sendMessage
-				({
-                    to: channelID,
-                    message: 'im old :older_man:'
-                });
-				break;	
-			case 'oliver':
-                bot.sendMessage
-				({
-                    to: channelID,
-                    message: 'olipieeeeeee :sob:'
-                });
-				break;
-            case 'nou':
-                bot.sendMessage
-				({
-                    to: channelID,
-                    message: 'no u'
-                });
-				break;
-			case 'qtpie':
-                bot.sendMessage
-				({
-                    to: channelID,
-                    message: 'hi there qtipies'
-                });
-				break;
-			case 'spikeynou':
-                bot.sendMessage
-				({
-                    to: channelID,
-                    message: ':spikey2nou:'
-                });
-				break;
-			case 'work':
-                bot.sendMessage
-				({
-                    to: channelID,
-                    message: 'toot tired after work today'
-                });
-				break;
-			case 'goodboy':
-                bot.sendMessage
-				({
-                    to: channelID,
-                    message: 'u are all good boys :sadcat:'
-                });
-				break;
-			case 'hate':
-                bot.sendMessage
-				({
-                    to: channelID,
-                    message: 'i do not like not cute people!!!'
-                });
-			break;
-            case 'chicky':
-				var chickyran = Math.floor(Math.random() * chickysaying.length); 
-				bot.sendMessage
-				({
-                    to: channelID,
-                    message: chickysaying[chickyran]
-                });
-			break;
+	//Automatically disregard capitalization from messages sent.
+	let msg = message.content.toUpperCase();
+
+	
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+	//INIT new player into database
+	var  star1 =0 , star2=0, star3=0, star4=0, star5=0, rolls=0, pityrate=0;
+	  // if the points don"t exist, init to 0;
+	if (!stats[message.author.id]) stats[message.author.id] = {
+		rolls: 0,
+		star1: 0,
+		star2: 0,
+		star3: 0,
+		star4: 0,
+		star5: 0,
+		pityrate: 0
+	};
+////////////////////////---- ECONOMY ------///////////////////////////////////////////////////////
+	if (msg.substring(0, 3) == 'ADD' && msg.substring(3, 4) == ' ')
+	{
+		var args = message.content.slice().split(" ");
+		//console.log(message.author);
+		//if (!message.member.roles.find("Administrator", modRole)){
+		//	message.channel.send('**You need admin role!');
+		//	return;
+		//}
+		if (!args[1]){
+			message.channel.send('You need to define an amount');
+			return;
+		}
+		if (isNaN(args[1])){
+			message.channel.send('You need to define an number');
+			return;
+		}
+		
+		let defineduser = '';
+		if(!args[2]){
+			defineduser = message.author.id;
+		}
+		else {
+			try{
+			let firstMentioned = message.mentions.users.first();
+			defineduser = firstMentioned.id;
 			}
-     }
+			catch(err){
+				console.log('id err');
+				return;
+			}
+		}
+		
+		economy.updateBalance(defineduser, args[1]).then((i) => {
+			message.channel.send(`**User defined had ${args[1]} added to their account.`)
+		});
+	
+	}
+		
+	if( msg == 'BALANCE' )
+	{
+		economy.fetchBalance(message.author.id).then((i) => {
+			const embed = new Discord.RichEmbed()
+				.setDescription(`**${message.guild.name} Bank**`)
+				.addField('Account Holder', message.author.username, true)
+				.addField('Account Balance', i.money, true)
+			message.channel.send({embed});
+	})
+	}	
+	
+	if (msg == 'GATCHA STATS'){
+		message.channel.send( message.author.username +' you have rolled, \nTotalRolls: '+ stats[message.author.id].rolls+ '\n☆☆☆☆☆: '+ stats[message.author.id].star5+ '\n☆☆☆☆: '+ stats[message.author.id].star4);
+		message.channel.send( '☆☆☆: '+ stats[message.author.id].star3+'\n☆☆: '+ stats[message.author.id].star2+'\n☆: '+ stats[message.author.id].star1);
+	}
+	if (msg == 'ROLL') 
+	{
+		economy.updateBalance(message.author.id, -10).then((i) => { 
+            message.channel.send(`**You paid for roll -10!**\n** ${message.author.username} New Balance:** ${i.money}`);
+        })
+		var starrandom = Math.floor(Math.random() * 99);
+		stats[message.author.id].rolls++;
+		starrandom = starrandom - stats[message.author.id].pityrate;
+		if (starrandom > 69 && starrandom <=99)
+		{
+			var random1 = Math.floor(Math.random() * star1pics);
+			stats[message.author.id].star1++;
+			message.channel.send
+			(
+				message.author.username +" you got\n 1 Star!", 
+				{file: '1/'+ random1 +'.png'}	
+			);
+			stats[message.author.id].pityrate++;
+			message.channel.send("Your pity rate is now: " + stats[message.author.id].pityrate + '% + 7% Base rate!');
+		}
+		if (starrandom > 41 && starrandom <=69)
+		{
+			var random2 = Math.floor(Math.random() * star2pics);
+			stats[message.author.id].star2++;
+			message.channel.send
+			(
+				message.author.username +" you got\n 2 Star", 
+				{file: '2/'+ random2 +'.png'}	
+			);
+			stats[message.author.id].pityrate++;
+			message.channel.send("Your pity rate is now: " + stats[message.author.id].pityrate + '% + 7% Base rate!');
+		}
+		if (starrandom > 20 && starrandom <=41)
+		{
+			var random3 = Math.floor(Math.random() * star3pics);
+			stats[message.author.id].star3++;
+			message.channel.send
+			(
+				message.author.username +" you got\n 3 Star", 
+				{file: '3/'+ random3 +'.png'}	
+			);
+			stats[message.author.id].pityrate++;
+			message.channel.send("Your pity rate is now: " + stats[message.author.id].pityrate + '% + 7% Base rate!');
+		}
+		if (starrandom > 6 && starrandom <=20)
+		{
+			var random4 = Math.floor(Math.random() * star4pics);
+			stats[message.author.id].star4++;
+			message.channel.send
+			(
+				message.author.username +" you got\n 4 Star", 
+				{file: '4/'+ random4 +'.png'}	
+			);
+			stats[message.author.id].pityrate++;
+			message.channel.send("Your pity rate is now: " + stats[message.author.id].pityrate + '% + 7% Base rate!');
+		}
+		if (starrandom <=6)
+		{
+			var random5 = Math.floor(Math.random() * star5pics);
+			stats[message.author.id].star5++;
+			message.channel.send
+			(
+				message.author.username +" you got\n 5 Stars", 
+				{file: 'waifus/5/'+ random5 +'.png'}	
+			);
+			stats[message.author.id].pityrate = 0;
+			message.channel.send("Your pity rate is now reset!: " + stats[message.author.id].pityrate + '%');
+		}
+		//Write new infomation into database
+		fs.writeFile("stats.json", JSON.stringify(stats), (err) => {
+		if (err) console.error(err)
+	});
+	}
+	
+	//////////////////////////////////////////////////////////////////
+	/*
+	if (msg === 'COMMAND HERE') 
+	{
+		message.channel.send
+		(
+			"", 
+			{
+			file: "FILEHERE.png" 
+			}
+		);
+	}
+	*/
+	
+///////////////////////////////////////////////// Emojis
+	if (msg === 'EMOJI HELP') {
+		message.channel.send
+		(
+		" surprise\n joy\n nerd\n smirk\n blush\n thinking\n "
+		);
+	}
+
+	if (msg === 'SURPRISE') {
+		message.channel.send
+		("",{file: "emoji/surprise.jpg"});
+	}
+	if (msg === 'SMIRK') {
+		message.channel.send
+		("",{file: "emoji/smirk.jpg"});
+	}
+	if (msg === 'JOY') {
+		message.channel.send
+		("", {file: "emoji/joy.jpg"});
+	}
+	if (msg === 'BLUSH') {
+		message.channel.send
+		("",{file: "emoji/blush.jpg"});
+	}
+	if (msg === 'THINKING') {
+		message.channel.send
+		("",{file: "emoji/thinking.jpg"});
+	}
+	
+//////////////////// END
 });
+
+// Log our bot in
+client.login(token);
